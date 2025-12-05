ee7063175046b70a1fe2dd242290fa6a83483e82
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-12-05 09:51:48 +0300

    feat: выполнить задание 1

---
+++ src/SubstringFunctions.java
@@ -0,0 +1,6 @@
+public class SubstringFunctions {
+    public boolean startsWith(String initial, String other) {
+        if (initial.indexOf(other) == 0) return true;
+        else return false;
+    }
+}
